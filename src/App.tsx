405875193d5594ea813afa9e09171b5233ef7390
xiaoxiaoxiannv <1017929760@qq.com>
2023-03-20 17:03:25 +0800

    引入vue-router

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { defineComponent, ref } from 'vue'
+
+export const App = defineComponent({
+  setup(){
+    return()=>(
+    <>
+    <header>
+      导航
+      <ul>
+        <li>
+        <router-link to='/'>Foo</router-link>
+        </li>
+        <li>
+      <router-link to='/about'>Bar</router-link>
+      </li>
+      </ul>
+    </header>
+    <div>
+      <router-view></router-view>
+    </div>
+    <footer>
+      页脚
+    </footer>
+    </>
+    )
+  }
+})
---
+++ src/views/Foo.tsx
@@ -0,0 +1,8 @@
+import { defineComponent} from 'vue';
+export const Foo = defineComponent({
+  setup: () => {
+    return () => (
+      <div>Foo</div>
+    )
+  }
+})
